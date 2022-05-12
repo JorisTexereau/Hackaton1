8077b37e180ab01e7cc8bb13a788b9d72b1f94f4
Joris TEXEREAU <joris-texereau_student2022@wilder.school>
2022-05-12 14:06:17 +0200

    creation coverPage avec barre de recherche + routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import Home from "@pages/Home";
-
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
