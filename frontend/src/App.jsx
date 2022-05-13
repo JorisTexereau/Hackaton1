93a842ca4a0e54ec11fbd9816879db42cafc58c5
MarcHazime <marc.hazime@gmail.com>
2022-05-13 09:45:14 +0200

    routes ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,18 @@ import Home from "@pages/Home";
 import Bordeaux from "@pages/Bordeaux";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Partenaires from "@pages/Partenaires";
+import Events from "@pages/Events";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/bordeaux" element={<Bordeaux />} />
+        <Route path="/events" element={<Events />} />
 
+        <Route path="/bordeaux" element={<Bordeaux />} />
+        <Route path="/partenaires" element={<Partenaires />} />
         <Route path="/Formulaire" element={<Form />} />
       </Routes>
     </div>
